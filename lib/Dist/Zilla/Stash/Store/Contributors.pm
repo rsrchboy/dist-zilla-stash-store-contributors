8bc2b81554d9482d487e80b213d28e3482b0bb3e
Chris Weyl <cweyl@alumni.drew.edu>
2014-05-14 14:17:00 -0700

    initial commit

---
+++ lib/Dist/Zilla/Stash/Store/Contributors.pm
@@ -0,0 +1,19 @@
+package Dist::Zilla::Stash::Store::Contributors;
+
+# ABSTRACT: The great new Dist::Zilla::Stash::Store::Contributors!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
